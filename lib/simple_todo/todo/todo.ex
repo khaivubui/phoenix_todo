5350903bc3950097339bcea335ca294c49e7a758
Khai Bui <khaivubui@gmail.com>
2017-11-05 23:36:20 -0800

    Add changeset to todo

--- lib/simple_todo/todo/todo.ex
+++ lib/simple_todo/todo/todo.ex
@@ -1,10 +1,18 @@
 defmodule Todo do
   use Ecto.Schema
+  import Ecto.Changeset
 
   schema "todos" do
     field :description, :string
     field :completed, :boolean, default: false
 
     timestamps()
+  end
+
+  def changeset todo, params \\ %{} do
+    todo
+      |> cast(params, [:description, :completed])
+      |> validate_required([:description])
+      |> validate_inclusion(:completed, [true, false])
   end
 end
