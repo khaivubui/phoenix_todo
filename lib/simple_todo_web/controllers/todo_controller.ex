95c009442251b75222b56ebce8e0bb4ab2c7d7f8
Khai Bui <khaivubui@gmail.com>
2017-11-05 23:40:06 -0800

    Add query for all todos to index

--- lib/simple_todo_web/controllers/todo_controller.ex
+++ lib/simple_todo_web/controllers/todo_controller.ex
@@ -1,7 +1,10 @@
 defmodule SimpleTodoWeb.TodoController do
   use SimpleTodoWeb, :controller
+  alias SimpleTodo.Repo
+  import Ecto.Query
 
   def index(conn, _params) do
+    todos = Repo.all(from t in Todo, order_by: t.id)
     render conn, :index
   end
 end
