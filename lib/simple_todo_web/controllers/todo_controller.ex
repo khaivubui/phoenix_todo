d15b60a8e224c060326446231756f190d75849e1
Khai Bui <khaivubui@gmail.com>
2017-11-06 19:45:36 -0800

    Add create action

--- lib/simple_todo_web/controllers/todo_controller.ex
+++ lib/simple_todo_web/controllers/todo_controller.ex
@@ -6,12 +6,27 @@ defmodule SimpleTodoWeb.TodoController do
   def index conn, _params do
     todos = Repo.all(from t in Todo, order_by: t.id)
     changeset = Todo.changeset %Todo{}
-    
+
     render conn, :index, todos: todos, changeset: changeset
   end
 
   def show conn, %{"id" => todo_id} do
     todo = Repo.get Todo, todo_id
     render conn, :show, todo: todo
+  end
+
+  def create conn, %{"todo" => todo} do
+    %Todo{}
+    |> Todo.changeset(todo)
+    |> Repo.insert()
+    |> case do
+      {:ok, _} ->
+        conn
+        |> put_flash(:info, "Todo Created")
+        |> redirect(to: todo_path(conn, :index))
+      {:error, changeset} ->
+        conn
+        |> render(:index, changeset: changeset)
+    end
   end
 end
